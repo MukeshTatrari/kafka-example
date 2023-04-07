395d790826ea252fae5bbbeb2c07ade1b3ec5500
MukeshTatrari <mukesh.tatrari@gmail.com>
2023-04-07 12:19:23 +0530

    kafka example

---
+++ src/main/java/com/kafka/api/kafka/JsonKafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.kafka.api.kafka;
+
+import com.kafka.api.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JsonKafkaConsumer {
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(User user){
+        log.info(String.format("Json message recieved -> %s", user.toString()));
+    }
+}
---
+++ src/main/java/com/kafka/api/model/User.java
@@ -0,0 +1,16 @@
+package com.kafka.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
